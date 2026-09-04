09669c534c7dc002f9ef6364dd43458146da74bf
isabelle <isacardenas28102008@outlook.com>
2026-09-03 21:06:56 -0300

    criando auth service

--- app/register.js
+++ app/register.js
@@ -4,6 +4,8 @@ import { router } from expo - router;
 import AppInput from '../src/components/AppInput';
 import AppButton from '../src/components/AppButton';
 import { COLORS } from "@/src/constants/theme";
+import { signUp } from "../src/services/authService";
+import {router} from 'expo-router';
 export default function Register() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -12,12 +14,24 @@ export default function Register() {
 
     //trim é uma função ou método usado para remover espaços em branco
     async function handleRegister() {
-        if (!email.trim()||!password||!confirm)
+        if (!email.trim() || !password || !confirm)
             return Alert.alert('Atenção', 'Preencha todos os campos');
-        if(password.length<6)
+        if (password.length < 6)
             return Alert.alert('Atenção', 'A senha deve ter no mínimo 6 caracteres');
-        if (password!==confirm)
+        if (password !== confirm)
             return Alert.alert('Atenção', 'As senhas não conferem');
+        try {
+            setLoading(true);
+            const { data, error } = await signUp(email.trim(), password);
+            if (error) {
+                Alert.alert('Erro', error.message);
+                console.log('Erro', error.message); return;}
+                if (data.session) router.replace();
+                else {
+                    Alert.alert('Cadastro realizado', 'Confirme seu e-mail, se necessário.'); router.replace('/');
+                }
+            } finally {setLoading(false);}
+        }
     }
 
     return (
@@ -27,21 +41,21 @@ export default function Register() {
             <AppInput label="E-mail"
                 //recusa letras maiusculas (autocapitalize) =>
                 autoCapitalize="none" keyboardType="email-address"
-            value={email} onChangeText={setEmail}
+                value={email} onChangeText={setEmail}
             />
             <AppInput label="Senha"
                 secureTextEntry
-            value={password} onChangeText={setPassword}
+                value={password} onChangeText={setPassword}
             />
             <AppInput label="Confirmar senha"
                 secureTextEntry
-            value={confirm} onChangeText={setConfirm}
+                value={confirm} onChangeText={setConfirm}
             />
             <AppButton
-                title="Criar conta" onPress={{handleRegister}}
-            loading={loading}
+                title="Criar conta" onPress={{ handleRegister }}
+                loading={loading}
             />
-           
+
         </KeyboardAvoidingView>
     );
 }
